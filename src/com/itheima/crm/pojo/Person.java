d7c47b796a23a4020c56c38b3b5c5a7b04e994e2
金友库 <869536360@qq.com>
2019-08-11 09:27:51 +0800

    添加person

---
+++ src/com/itheima/crm/pojo/Person.java
@@ -0,0 +1,40 @@
+package com.itheima.crm.pojo;
+
+public class Person {
+    private int id;
+    private int eag;
+    private String name;
+
+    public Person() {
+    }
+
+    public Person(int id, int eag, String name) {
+        this.id = id;
+        this.eag = eag;
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getEag() {
+        return eag;
+    }
+
+    public void setEag(int eag) {
+        this.eag = eag;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
